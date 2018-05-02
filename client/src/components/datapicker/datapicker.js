0955c9849542ed058c4f8d4b5c65ff59ad3d31dd
sphynx79 <miboscol@gmail.com>
2018-05-02 13:01:11 +0200

    Added: Aggiunto freccette per scorrere i giorni

--- client/src/components/datapicker/datapicker.js
+++ client/src/components/datapicker/datapicker.js
@@ -26,14 +26,41 @@ class DataPicker {
                 m(".bx--date-picker-container", [
                     m(calendarIcon),
                     m("input.bx--date-picker__input[data-date-picker-input=''][id='date-picker'][pattern='d{1,2}/d{1,2}/d{4}'][placeholder='dd/mm/yyyy'][type='text']"),
-                    m(".bx--form-requirement", "Invalid date format."),
+                    m(".bx--number__controls", [
+                        m("button.bx--number__control-btn.up-icon",{onclick: () => this.changeData(1)}, 
+                          m("svg[fill-rule='evenodd'][height='5'][viewBox='0 2 10 5'][width='10']", 
+                            m("path[d='M10 5L5 0 0 5z']")
+                          )
+                        ),
+                        m("button.bx--number__control-btn.down-icon", {onclick: () => this.changeData(-1)}, 
+                          m("svg[fill-rule='evenodd'][height='5'][viewBox='0 2 10 5'][width='10']", 
+                            m("path[d='M10 0L5 5 0 0z']")
+                          )
+                        )
+                    ]),
+                    m(".bx--form-requirement", "Invalid date format.")
                 ])
             ),
         ])
     }
 
+    changeData(incremento) {
+        let currentDate = new Date(appState.$data.get().replace(/-/g, "/"))
+        currentDate.setDate(currentDate.getDate() + incremento)
+        let dataStrPicker = `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}`
+        let dataStr = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`
+        this.picker.calendar.setDate(dataStrPicker)
+        appState.$data.set(dataStr)
+    }
+
     oncreate(vnode) {
         let el = vnode.dom.lastChild
+        console.log(el)
+
+        // debugger
+        // console.log(el)
+        // let up = el.getElementsByClassName("up-icon")[0]
+        // let down = el.getElementsByClassName("down-icon")[0]
 
         vnode.picker = DatePicker.create(el, {
             dateFormat: "d-m-Y",
@@ -42,8 +69,10 @@ class DataPicker {
         })
 
         vnode.picker.calendar.config.onChange.push((selectedDates, dateStr, instance) => {
+            console.log(dateStr)
             appState.$data.set(dateStr)
         })
+        vnode.state.picker = vnode.picker
 
         if (process.env.NODE_ENV !== "production") {
             let logStateAttrs = {
