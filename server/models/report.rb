9a2cf1d363097105ee7bd9805d743e37657a00b8
sphynx79 <miboscol@gmail.com>
2018-10-01 18:14:10 +0200

    nothing

--- server/models/report.rb
+++ server/models/report.rb
@@ -28,24 +28,29 @@ class Report
       # zona_hourly = []
       # threads = []
       # threads << Thread.new do
-         tec_hourly = DB[:remit_centrali_hourly_tecologia].aggregate(pipeline_hourly_tecologia(start_dt, end_dt)).allow_disk_use(true).to_a
+      # tec_hourly = DB[:remit_centrali_hourly_tecologia].aggregate(pipeline_hourly_tecologia(start_dt, end_dt)).allow_disk_use(true).to_a
       # end
       # threads << Thread.new do
-         zona_hourly = DB[:remit_centrali_hourly_zona].aggregate(pipeline_hourly_zona(start_dt, end_dt)).allow_disk_use(true).to_a
+      # zona_hourly = DB[:remit_centrali_hourly_zona].aggregate(pipeline_hourly_zona(start_dt, end_dt)).allow_disk_use(true).to_a
       # end
       # threads.each do |t|
       #   t.join
       # end
+      # tec_hourly = DB[:remit_centrali_hourly_tecologia].aggregate(pipeline_hourly_tecologia(start_dt, end_dt)).allow_disk_use(true).to_a
+      # zona_hourly = DB[:remit_centrali_hourly_zona].aggregate(pipeline_hourly_zona(start_dt, end_dt)).allow_disk_use(true).to_a
+      # remit_tec_daily, remit_zona_daily = remit_daily(tec_hourly, zona_hourly)
+      # remit = {}
+      # remit[:tec_hourly] = tec_hourly
+      # remit[:zona_hourly] = zona_hourly
+      # remit[:tec_daily] = remit_tec_daily
+      # remit[:zona_daily] = remit_zona_daily
       
-     
-      
-      remit_tec_daily, remit_zona_daily = remit_daily(tec_hourly, zona_hourly)
 
       remit = {}
-      remit[:tec_hourly] = tec_hourly
-      remit[:zona_hourly] = zona_hourly
-      remit[:tec_daily] = remit_tec_daily
-      remit[:zona_daily] = remit_zona_daily
+      remit[:tec_hourly] = DB[:remit_centrali_hourly_tecologia].aggregate(pipeline_hourly_tecologia(start_dt, end_dt)).allow_disk_use(true).to_a
+      remit[:zona_hourly] = DB[:remit_centrali_hourly_zona].aggregate(pipeline_hourly_zona(start_dt, end_dt)).allow_disk_use(true).to_a
+      remit[:tec_daily] = DB[:remit_centrali_daily_tecologia].aggregate(pipeline_daily_tecologia(start_dt, end_dt)).allow_disk_use(true).to_a
+      remit[:zona_daily] = DB[:remit_centrali_daily_zona].aggregate(pipeline_daily_zona(start_dt, end_dt)).allow_disk_use(true).to_a
 
       remit
     end
@@ -150,6 +155,85 @@ class Report
           sard: "$sard",
           sici: "$sici",
           sud: "$sud",
+        },
+      }
+    end
+
+    def pipeline_daily_zona(start_dt, end_dt)
+      pipeline = []
+      # pipeline << {:$match => {"event_status": "Active"}}
+      # pipeline << {:$match => {"dt_start": {:$lte => end_dt}, "dt_end": {:$gte => start_dt}}}
+      pipeline << {
+        "$match": {
+          "$and": [
+            {
+              "dataTime": {
+                "$lte": end_dt,
+              },
+            }, {
+              "dataTime": {
+                "$gte": start_dt,
+              },
+            },
+          ],
+        },
+      }
+      pipeline << {
+        "$project": {
+          _id: 0,
+          data: {"$dateToString": {
+            format: "%Y-%m-%d",
+            date: "$dataTime",
+          # timezone: "Europe/Rome",
+          }},
+          brnn: "$brnn",
+          cnor: "$cnor",
+          csud: "$csud",
+          fogn: "$fogn",
+          nord: "$nord",
+          prgp: "$prgp",
+          rosn: "$rosn",
+          sard: "$sard",
+          sici: "$sici",
+          sud: "$sud",
+        },
+      }
+    end
+
+    def pipeline_daily_tecologia(start_dt, end_dt)
+      pipeline = []
+      # pipeline << {:$match => {"event_status": "Active"}}
+      # pipeline << {:$match => {"dt_start": {:$lte => end_dt}, "dt_end": {:$gte => start_dt}}}
+      pipeline << {
+        "$match": {
+          "$and": [
+            {
+              "dataTime": {
+                "$lte": end_dt,
+              },
+            }, {
+              "dataTime": {
+                "$gte": start_dt,
+              },
+            },
+          ],
+        },
+      }
+      pipeline << {
+        "$project": {
+          _id: 0,
+          data: {"$dateToString": {
+            format: "%Y-%m-%d",
+            date: "$dataTime",
+          # timezone: "Europe/Rome",
+          }},
+          termico: "$termico",
+          pompaggio: "$pompaggio",
+          autoproduttore: "$autoproduttore",
+          idrico: "$idrico",
+          eolico: "$eolico",
+          solare: "$solare",
+          geotermico: "$geotermico",
         },
       }
     end
