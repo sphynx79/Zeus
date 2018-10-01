831817953958149c69c06060140ae8401d48ba78
sphynx79 <miboscol@gmail.com>
2018-10-01 21:08:01 +0200

    nothing

--- server/routes/api/v1/report.rb
+++ server/routes/api/v1/report.rb
@@ -2,16 +2,6 @@
 
 class Ampere
   route("report", "api/v1") do |r|
-    r.get "centrali", String, String do |start_dt, end_dt|
-      start_dt = Time.parse(start_dt)
-      utc_offset_start_dt = start_dt.utc_offset
-      end_dt = Time.parse(end_dt)
-      utc_offset_end_dt = end_dt.utc_offset
-
-      start_dt = (start_dt + utc_offset_start_dt).utc
-      end_dt = (end_dt + utc_offset_end_dt + (3600 * 24) - 1).utc
-      Report.get_report(start_dt, end_dt)
-    end
     r.get "centrali_tecnologia", String, String do |start_dt, end_dt|
       start_dt =  Time.parse(start_dt)
       end_dt = (Time.parse(end_dt) + (3600 * 24) - 1)
