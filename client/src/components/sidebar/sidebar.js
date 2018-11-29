1f5f23927a8aa636b3edb3e91ef8ef78f29d6c13
sphynx79 <miboscol@gmail.com>
2018-11-29 19:15:34 +0100

    Unificato in un unico componente le table

--- client/src/components/sidebar/sidebar.js
+++ client/src/components/sidebar/sidebar.js
@@ -1,9 +1,7 @@
 // src/components/sidebar/sidebar.js
 
 import "./sidebar.scss"
-import TableCentrali from "components/tablecentrali/tablecentrali.js"
-import TableLinee from "components/tablelinee/tablelinee.js"
-// import Table from "components/table/table.js"
+import Table from "components/table/table.js"
 import Filtri from "components/filtri/filtri.js"
 
 class SideBar {
@@ -19,9 +17,9 @@ class SideBar {
             return m("nav.sidebar#sidebar_right", attrs, [appState.$lista_centrali.get() === undefined ? "" : m(Filtri)])
         } else {
             return m("nav.sidebar#sidebar_left", attrs, [
-                appState.$remit_380.get() === undefined ? "" : m(TableLinee, { type: "linee", volt: "380", remit: appState.$remit_380  }),
-                appState.$remit_220.get() === undefined ? "" : m(TableLinee, { type: "linee", volt: "220", remit: appState.$remit_220 }),
-                appState.$remit_centrali.get() === undefined ? "" : m(TableCentrali, { type: "centrali", remit: appState.$remitCentraliFiltered }),
+                appState.$remit_380.get()      === undefined ? "" : m(Table, { type: "linee",    volt: "380", remit: appState.$remit_380  }),
+                appState.$remit_220.get()      === undefined ? "" : m(Table, { type: "linee",    volt: "220", remit: appState.$remit_220 }),
+                appState.$remit_centrali.get() === undefined ? "" : m(Table, { type: "centrali", remit: appState.$remitCentraliFiltered }),
             ])
         }
     }
