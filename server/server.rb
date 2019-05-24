47411bae1808ac937b2d7ea327dfb0f0c6bb67fa
sphynx79 <miboscol@gmail.com>
2019-05-24 22:04:02 +0200

    Aggiornato configurazione rubocop

--- server/server.rb
+++ server/server.rb
@@ -1,20 +1,19 @@
 #!/usr/bin/env ruby
-# Encoding: utf-8
 # warn_indent: true
 # frozen_string_literal: true
 
 APP_ROOT = Pathname.new(File.expand_path(__dir__)).freeze
 APP_NAME = APP_ROOT.basename.to_s.freeze
-VERSION = File.read("../VERSION").strip
+VERSION = File.read('../VERSION').strip
 
 require 'tzinfo'
-ENV["TZ"] = "UTC"
+ENV['TZ'] = 'UTC'
 TZ = TZInfo::Timezone.get('Europe/Rome')
 
-env = ENV["RACK_ENV"] || "development"
+env = ENV['RACK_ENV'] || 'development'
 
-ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __FILE__)
-require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 Bundler.require(:default, env)
 
 require_relative 'app/initializers/settings'
@@ -22,7 +21,7 @@ require_relative 'app/boot'
 require_relative 'app/boot_jobs'
 require_glob APP_ROOT.to_s + '/app/routes/*.rb'
 
-Logger.class_eval { alias :write :'<<' }
+Logger.class_eval { alias_method :write, :<< }
 
 class Server < Roda
   plugin :environments
@@ -33,29 +32,27 @@ class Server < Roda
   include Logging
 
   configure :development do
-    plugin :common_logger, Logging.logger 
-    puts "#" * 80
-    puts "Start Development mode"
+    plugin :common_logger, Logging.logger
+    puts '#' * 80
+    puts 'Start Development mode'
     puts "Version: #{VERSION}"
-    puts "ip: localhost:9292"
-    puts "ip db: #{Settings.database.adress.join(", ")}"
-    puts "#" * 80
+    puts 'ip: localhost:9292'
+    puts "ip db: #{Settings.database.adress.join(', ')}"
+    puts '#' * 80
   end
 
   configure :production do
-    # ip = Socket.ip_address_list.keep_if { |intf| intf.ipv4_private? && (intf.ip_address =~ /^10/ || intf.ip_address =~ /^192/) }[0].ip_address
-    # ip = Socket::getaddrinfo(Socket.gethostname,"echo",Socket::AF_INET)[0][3]
     adress = Socket.gethostname
-    puts "#" * 80
-    puts "Start Production mode"
+    puts '#' * 80
+    puts 'Start Production mode'
     puts "* Version: #{VERSION}"
     puts "* Application avviable at: https://#{adress}:2015"
-    puts "* DB avviable at : #{Settings.database.adress.join(", ")}"
-    puts "#" * 80
+    puts "* DB avviable at : #{Settings.database.adress.join(', ')}"
+    puts '#' * 80
     # use Rack::Cache, verbose: false
     # use Rack::Brotli, :if => lambda { |env, status, headers, body| headers["Content-Length"].to_i > 3600 }
     # IMPORTANTE: Uso deflate al posto di brotli perchè dai test mi e risultava piu performante
-    use Rack::Deflater, :if => lambda { |env, status, headers, body| headers["Content-Length"].to_i > 3600 }
+    use Rack::Deflater, if: ->(_env, _status, headers, _body) { headers['Content-Length'].to_i > 3600 }
     # use Rack::RubyProf, :path => 'profile', :printers => [::RubyProf::CallTreePrinter]
     # set :public_folder, 'public'
   end
@@ -64,15 +61,13 @@ class Server < Roda
     r.public
 
     r.root do
-      response["Content-Type"] = "text/html"
-      File.read(File.join(APP_ROOT, "public/index.html"))
+      response['Content-Type'] = 'text/html'
+      File.read(File.join(APP_ROOT, 'public/index.html'))
     end
 
-    r.on "api/v1" do
+    r.on 'api/v1' do
       r.run V1::Api
     end
-
   end
-
 end
 
